34897df99a6bf65b38ed47baa1bec240fd80a234
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-09-01 14:34:31 +0530

    Added fetch forms api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
-  <router-view></router-view>
+  <transition name="fade">
+    <router-view></router-view>
+  </transition>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
 
 <script>
 export default {
--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-col style="padding-top: 60px; padding-left: 60px; padding-right: 60px;">
       <v-row>
-        <v-card shaped class="form-card" to="/forms/create">
+        <v-card shaped class="form-card" to="/forms/create" elevation="5">
           <v-row align="center" justify="center" style="height: 205px;">
             <v-col>
               <v-row align="center" justify="center">
@@ -12,7 +12,23 @@
             </v-col>
           </v-row>
         </v-card>
-        <!-- <v-card v-for="item in items" :key="item" shaped class="form-card">Create New Form</v-card> -->
+        <v-card
+          v-for="form in forms"
+          :key="form.id"
+          shaped
+          class="form-card"
+          :to="'/forms/' + form.id + '/edit'"
+          elevation="5"
+        >
+          <v-row align="center" justify="center" style="height: 205px;">
+            <v-col>
+              <v-row align="center" justify="center">
+                <v-icon style="font-size:80px" :color="themeColor">mdi-plus</v-icon>
+              </v-row>
+              <v-row align="center" justify="center" class="mt-2">{{form.title}}</v-row>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-row>
     </v-col>
   </v-container>
@@ -28,18 +44,29 @@
 </style>
 
 <script>
-// import store from "../store";
 import ThemeMixin from "../mixins/ThemeMixin";
 export default {
   name: "AppBody",
   mixins: [ThemeMixin],
-  data: () => ({}),
-  methods: {},
-
-  computed: {
-    // themeColor: () => {
-    //   return store.state.theme.value;
-    // },
+  data: () => ({
+    forms: [],
+  }),
+  mounted: async function () {
+    let forms = await this.fetchForms();
+    console.log(forms);
+    this.forms = [...this.forms, ...forms.list];
   },
+  methods: {
+    async fetchForms() {
+      let response = await this.axios.get("http://localhost:3000/forms");
+      if (response.status === 200) {
+        if (response.data && response.data.code === 200) {
+          return response.data.data;
+        }
+      } else throw alert("Error");
+    },
+  },
+
+  computed: {},
 };
 </script>
