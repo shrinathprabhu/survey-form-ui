ef25ac1d0b75fed55c93ba22c5aa65e91f4d980f
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-09-08 11:18:57 +0530

    Added add question component

--- src/components/EditForm/QuestionComponent.vue
+++ src/components/EditForm/QuestionComponent.vue
@@ -1,16 +1,89 @@
 <template>
   <v-container>
-    <v-card>
-      <v-col class="px-5">
-        <v-text-field
-          flat
-          v-model="formDetails.title"
-          :color="themeColor"
-          :loading="fieldsLoading"
-          class="py-5 form-title"
-        ></v-text-field>
+    <v-row>
+      <v-col class="mr-5">
+        <v-card shaped elevation="2" outlined class="pb-5 mb-5">
+          <v-col class="px-6" style="padding-bottom: 0">
+            <v-text-field
+              flat
+              v-model="formDetails.title"
+              :color="themeColor"
+              height="50px"
+              :loading="fieldsLoading"
+              class="form-title"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-col>
+          <v-col class="px-6">
+            <v-text-field
+              flat
+              v-model="formDetails.description"
+              :color="themeColor"
+              placeholder="Description"
+              :loading="fieldsLoading"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-col>
+        </v-card>
+        <v-card
+          elevation="2"
+          outlined
+          class="mt-3 mb-3"
+          v-for="(question, index) in formDetails.questionnaires"
+          :key="index"
+        >
+          <QuestionFields :question="question" />
+        </v-card>
       </v-col>
-    </v-card>
+      <!-- <v-layout row wrap> -->
+      <v-card elevation="3" style="position: fixed; right: 10px">
+        <v-col class="px-5">
+          <v-row class="py-1">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on" icon elevation="0" @click.stop="addQuestion()">
+                  <v-icon>mdi-plus-circle-outline</v-icon>
+                </v-btn>
+              </template>
+              <span>Add new question</span>
+            </v-tooltip>
+          </v-row>
+          <v-row class="py-1">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on" icon elevation="0">
+                  <v-icon>mdi-format-page-break</v-icon>
+                </v-btn>
+              </template>
+              <span>Add new section</span>
+            </v-tooltip>
+          </v-row>
+          <v-row class="py-1">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on" icon elevation="0">
+                  <v-icon>mdi-cogs</v-icon>
+                </v-btn>
+              </template>
+              <span>Form settings</span>
+            </v-tooltip>
+          </v-row>
+          <v-row class="py-1">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on" icon elevation="0">
+                  <v-icon>mdi-export-variant</v-icon>
+                </v-btn>
+              </template>
+              <span>Export as JSON</span>
+            </v-tooltip>
+          </v-row>
+        </v-col>
+      </v-card>
+      <!-- </v-layout> -->
+    </v-row>
   </v-container>
 </template>
 
@@ -22,13 +95,25 @@
 
 <script>
 import ThemeMixin from "../../mixins/ThemeMixin";
+import QuestionFields from "./QuestionFields";
 export default {
   name: "QuestionComponent",
   mixins: [ThemeMixin],
+  components: { QuestionFields },
   props: ["formDetails"],
   computed: {
     fieldsLoading: function () {
       return this.formDetails ? false : true;
+    },
+  },
+  methods: {
+    addQuestion: function () {
+      this.formDetails = this.formDetails || {};
+      this.formDetails.questionnaires = this.formDetails.questionnaires || [];
+      this.formDetails.questionnaires.push({
+        question: "Untitled Question",
+        answerType: "Short answer",
+      });
     },
   },
 };
---
+++ src/components/EditForm/QuestionFields.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-col>
+    <v-row class="px-5 py-2">
+      <v-col cols="9">
+        <v-text-field flat :color="themeColor" v-model="question.question"></v-text-field>
+      </v-col>
+      <v-col cols="3">
+        <v-select
+          :color="themeColor"
+          placeholder="Answer Type"
+          v-model="answerType"
+          :items="answerTypes"
+          cache-items
+          :item-color="themeColor"
+        >
+          <template v-slot:selection="data">
+            <v-icon :color="themeColor" class="px-2">{{data.item.icon}}</v-icon>
+            <span>{{data.item.value}}</span>
+          </template>
+          <template v-slot:item="data">
+            <v-icon :color="themeColor" class="px-2">{{data.item.icon}}</v-icon>
+            <span>{{data.item.value}}</span>
+          </template>
+        </v-select>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+import ThemeMixin from "../../mixins/ThemeMixin";
+export default {
+  name: "QuestionFields",
+  props: ["question"],
+  mixins: [ThemeMixin],
+  data: () => ({
+    answerTypes: [
+      {
+        icon: "mdi-comment-text-outline",
+        value: "Short answer",
+        text: "Short answer",
+      },
+      {
+        icon: "mdi-comment-text-outline",
+        value: "Paragraph",
+        text: "Paragraph",
+      },
+      {
+        icon: "mdi-radiobox-marked",
+        value: "Multiple choice",
+        text: "Multiple choice",
+      },
+      { icon: "mdi-checkbox-marked", value: "Checkbox", text: "Checkbox" },
+      {
+        icon: "mdi-arrow-down-drop-circle",
+        value: "Dropdown",
+        text: "Dropdown",
+      },
+      { icon: "mdi-comment-text-outline", value: "Range", text: "Range" },
+      { icon: "mdi-calendar", value: "Date", text: "Date" },
+      { icon: "mdi-timer", value: "Time", text: "Time" },
+    ],
+  }),
+  computed: {
+    answerType: function () {
+      return this.question.answerType;
+    },
+  },
+};
+</script>
