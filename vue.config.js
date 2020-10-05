4e940b83a9c3311f67c5535d2cb5479cd7370650
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-10-05 09:30:44 +0530

    Stopped bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
     configureWebpack: {
-        plugins: [new BundleAnalyzerPlugin()]
+        plugins: [
+            // new BundleAnalyzerPlugin()
+        ]
     }
 };
