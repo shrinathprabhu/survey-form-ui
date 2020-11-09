f1d7b6161b52004285a1b1031333f1d196b6a469
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-11-09 22:48:08 +0530

    db.js lint

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,25 @@
-import Zango from 'zangodb';
+import Zango from "zangodb";
 
-// if (!('indexedDB' in window)) {
-//     global.indexedDB = require('fake-indexeddb');
-//     global.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange');
-// }
-let db = new Zango.Db('openforms', ['forms']);
+if (!("indexedDB" in window)) {
+  global.indexedDB = require("fake-indexeddb");
+  global.IDBKeyRange = require("fake-indexeddb/lib/FDBKeyRange");
+}
+let db = new Zango.Db("openforms", ["forms"]);
 
 export async function fetchForms() {
-    return await db.collection('forms').find().toArray();
+  return await db
+    .collection("forms")
+    .find()
+    .toArray();
 }
 
 export async function saveForms(forms) {
-    await db.collection('forms').remove({});
-    await db.collection('forms').insert(forms);
-    return;
+  await db.collection("forms").remove({});
+  await db.collection("forms").insert(forms);
+  return;
 }
 
 export default {
-    fetchForms,
-    saveForms
-}
+  fetchForms,
+  saveForms,
+};
