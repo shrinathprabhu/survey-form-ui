ff9185b7de7fb08245fc74ff96bf9e61c7df4562
Shrinath Prabhu <shrinath.prabhu@hashbinary.com>
2020-11-09 22:49:14 +0530

    Added go to dashboard button to 404 and 500

--- src/views/page-not-found.vue
+++ src/views/page-not-found.vue
@@ -6,10 +6,12 @@
           <v-row justify="center" align="center" class="mt-3 pt-3">
             <h1 :color="themeColor">Error 404: Page not found</h1>
           </v-row>
-          <v-row justify="center" align="center" class="mt-3 pt-3"
-            >Oops. Seems like the page you're trying to access does not
-            exist</v-row
-          >
+          <v-row justify="center" align="center" class="mt-3 pt-3">
+            Oops. Seems like the page you're trying to access does not exist
+          </v-row>
+          <v-btn to="/forms" :color="themeColor" tile dark class="mt-5">
+            Go to Dashboard
+          </v-btn>
         </v-col>
       </v-container>
     </v-main>
@@ -21,6 +23,9 @@ import ThemeMixin from "../mixins/theme-mixin";
 export default {
   name: "page-not-found",
   mixins: [ThemeMixin],
+  mounted() {
+    document.title = "Error 404 - Page not found";
+  },
   data: () => ({}),
   methods: {},
 };
--- src/views/something-went-wrong.vue
+++ src/views/something-went-wrong.vue
@@ -6,10 +6,13 @@
           <v-row justify="center" align="center" class="mt-3 pt-3">
             <h1 :color="themeColor">Error 500: Something went wrong</h1>
           </v-row>
-          <v-row justify="center" align="center" class="mt-3 pt-3"
-            >Maybe this is an internal server issue or maybe you're trying to
-            access invalid or inaccessible pages</v-row
-          >
+          <v-row justify="center" align="center" class="mt-3 pt-3">
+            Maybe this is an internal server issue or maybe you're trying to
+            access invalid or inaccessible pages
+          </v-row>
+          <v-btn to="/forms" :color="themeColor" tile dark class="mt-5">
+            Go to Dashboard
+          </v-btn>
         </v-col>
       </v-container>
     </v-main>
@@ -21,6 +24,9 @@ import ThemeMixin from "../mixins/theme-mixin";
 export default {
   name: "something-went-wrong",
   mixins: [ThemeMixin],
+  mounted() {
+    document.title = "Error 500 - Something went wrong";
+  },
   data: () => ({}),
   methods: {},
 };
